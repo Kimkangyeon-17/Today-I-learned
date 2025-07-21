90607a3ae10906c0657c9324776bb5937b7e2f65
kimkangyeon-17 <rkddus0517@naver.com>
2025-07-21 14:12:35 +0900

    실습 문제 풀이

---
+++ 0721/python_ws_1_5/ws_1_5.py
@@ -0,0 +1,15 @@
+print(3 * 2)
+print(3 ** 2)
+print(3 ** 2 // (3*2))
+print(3 ** 2 % (3*2))
+print(3 ** 2 + (-3) ** 2)
+
+mul = 3 * 2
+sqr = 3 ** 2
+minus_mul = (-3) ** 2
+
+print(mul)
+print(sqr)
+print(sqr // mul)
+print(sqr % mul)
+print(sqr + minus_mul)
